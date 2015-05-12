3c7da4bca1c71b5538a5a0b548afd10d39576d83
Charles <Charles@Apollo>
2015-05-12 01:32:39 -0400

    Added more ways

--- src/com/staalcomputingsolutions/duplicateremover/DuplicateRemover.java
+++ src/com/staalcomputingsolutions/duplicateremover/DuplicateRemover.java
@@ -46,7 +46,7 @@ public class DuplicateRemover {
                 }
             }
             System.out.println(removeDuplicateVONE(toBeRemoved, theString));
-            System.out.println(removeDuplicateVTWO(toBeRemoved, theString));
+            System.out.println(removeDuplicateVTHREE(toBeRemoved, theString));
         }
     }
 
@@ -64,45 +64,45 @@ public class DuplicateRemover {
         return theString;
     }
 
-    public static String removeDuplicateVTWO(String toBeRemoved, String theString) {
-        char[] newString = new char[theString.length()];
-        List<Integer> indexes = new LinkedList();
-        char currentChar;
-        int index;
-        for (int rsi = 0; rsi < toBeRemoved.length(); rsi++) {
-            currentChar = toBeRemoved.charAt(rsi);
-            for (int tsi = 0; tsi < theString.length() - 1; tsi++) {
-                if (theString.charAt(tsi) == currentChar) {
-                    indexes.add(tsi);
-                }
-            }
-            for (int x = indexes.size() - 1; x > 0; x--) {
-                index = indexes.get(x);
-                newString[index] = theString.charAt(x);
-            }
-            indexes.clear();
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int x = 0; x < newString.length; x++) {
-            if (newString[x] != '\0') {
-                sb.append(newString[x]);
-            }
-        }
-        return sb.toString();
-    }
-
+    /*
+     public static String removeDuplicateVTWO(String toBeRemoved, String theString) {
+     char[] newString = new char[theString.length()];
+     List<Integer> indexes = new LinkedList();
+     char currentChar;
+     int index;
+     for (int rsi = 0; rsi < toBeRemoved.length(); rsi++) {
+     currentChar = toBeRemoved.charAt(rsi);
+     for (int tsi = 0; tsi < theString.length() - 1; tsi++) {
+     if (theString.charAt(tsi) == currentChar) {
+     indexes.add(tsi);
+     }
+     }
+     for (int x = indexes.size() - 1; x > 0; x--) {
+     index = indexes.get(x);
+     newString[index] = theString.charAt(x);
+     }
+     indexes.clear();
+     }
+     StringBuilder sb = new StringBuilder();
+     for (int x = 0; x < newString.length; x++) {
+     if (newString[x] != '\0') {
+     sb.append(newString[x]);
+     }
+     }
+     return sb.toString();
+     }*/
     public static String removeDuplicateVTHREE(String toBeDeleted, String theString) {
 
         char currentChar;
         char[] theStringCharArray = theString.toCharArray();
         boolean foundFirst;
-        
-        for(int index0 = 0; index0 < toBeDeleted.length(); index0++){
+
+        for (int index0 = 0; index0 < toBeDeleted.length(); index0++) {
             currentChar = toBeDeleted.charAt(index0);
             foundFirst = false;
-            for(int index1 = 0; index1 < theStringCharArray.length; index1++){
-                if(theStringCharArray[index1] == currentChar ){
-                    if(!foundFirst){
+            for (int index1 = 0; index1 < theStringCharArray.length; index1++) {
+                if (theStringCharArray[index1] == currentChar) {
+                    if (foundFirst) {
                         theStringCharArray[index1] = '\0';
                     } else {
                         foundFirst = true;
@@ -110,6 +110,12 @@ public class DuplicateRemover {
                 }
             }
         }
-        return theStringCharArray.toString();
+        StringBuilder sb = new StringBuilder();
+        for (int index2 = 0; index2 < theStringCharArray.length; index2++) {
+            if (theStringCharArray[index2] != '\0') {
+                sb.append(theStringCharArray[index2]);
+            }
+        }
+        return sb.toString();
     }
 }
